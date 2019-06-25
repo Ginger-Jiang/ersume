6bfad70f05efb1610c6d0078fce8dda2de9f1e81
Ginger <ziqingnanyi@gmail.com>
2019-06-25 09:04:37 +0800

    add topNavVar transition

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-// window.onload = function () {
-//   var loding = document.getElementById('loding')
-//   loding.classList.remove('active')
-// }
+// loding
+setTimeout(() => {
+  var loding = document.getElementById('loding')
+  loding.classList.remove('active')
+}, 1000);
+
+
+window.onscroll = function (e) {
+  let element = document.getElementById('topNavBar')
+  let scrollY = window.scrollY
+  if (scrollY > 0) {
+    element.classList.add('sticky')
+  } else {
+    element.classList.remove('sticky')
+  }
+}
